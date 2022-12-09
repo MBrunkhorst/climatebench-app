c4c7917f233bf057095d36e1d777e7fa8c1bb62a
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2022-12-09 10:53:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,6 +90,10 @@ def main():
     x, y = np.linspace(0, 360, 144), np.linspace(-90, 90, 96)
     dataset = xr.DataArray(temperature, coords={'latitude': (('latitude',), y), 'longitude': (('longitude',), x)})
 
+    #     # Draw the header and image.
+    st.subheader("Real-time Climate Simulations")
+    st.markdown("**ClimateBench Emulator** (CO2 `%3.1f`) (CH4 `%3.1f`)" % (co2, ch4))
+    st.markdown(f"Global mean temperature change: {global_mean(dataset):3.1f}K +/- {uncertainty:3.1f}K")
     
     ## FIXME: Using a cartopy proijection breaks streamlit...
 #     fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(11,8.5))
@@ -99,10 +103,6 @@ def main():
 
     st.pyplot(fig)
     
-#     # Draw the header and image.
-    st.subheader("Real-time Climate Simulations")
-    st.markdown("**ClimateBench Emulator** (CO2 `%3.1f`) (CH4 `%3.1f`)" % (co2, ch4))
-    st.markdown(f"Global mean temperature change: {global_mean(dataset):3.1f}K +/- {uncertainty:3.1f}K")
 
 #     air_temperature = gv.Dataset(dataset, ['longitude', 'latitude'], 'air_temperature')
 # #     fig = gv.render((), backend='bokeh')

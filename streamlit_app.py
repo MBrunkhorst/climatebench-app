accc78dd306b199c3158fe556eb4af77a3192acf
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2022-12-09 10:21:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,8 +90,10 @@ def main():
     x, y = np.linspace(0, 360, 144), np.linspace(-90, 90, 96)
     dataset = xr.DataArray(temperature, coords={'latitude': (('latitude',), y), 'longitude': (('longitude',), x)})
 
-    fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(11,8.5))
-#     fig, ax = plt.subplots()
+    
+    ## FIXME: Using a cartopy proijection breaks streamlit...
+#     fig, ax = plt.subplots(subplot_kw={'projection': ccrs.PlateCarree()}, figsize=(11,8.5))
+    fig, ax = plt.subplots()
     
     dataset.plot(ax=ax, cmap='coolwarm', vmax=6.)
 
